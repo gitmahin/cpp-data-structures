b4fd43d2f2903bd3805f779175946f943659565f
gitmahin <nimahin25@gmail.com>
2026-08-03 20:30:09 +0600

    feat: move algorithms

---
+++ examples/sort.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "../sort.h"
+
+int main()
+{
+    int arr[] = {42, 7, 91, 15, 63, 28, 3, 77, 50, 19};
+    
+    Sort sort;
+    Utils utils;
+
+    std::cout << "Before sort:" << std::endl;
+    utils.printArray(arr);
+    sort.bubbleSort(arr);
+    std::cout << "After sort:" << std::endl;
+    utils.printArray(arr);
+}
